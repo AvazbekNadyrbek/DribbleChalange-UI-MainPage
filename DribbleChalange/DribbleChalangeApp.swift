6e4d86563f9d53066df3e533650a397cab6bfe9d
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-14 13:18:50 +0200

    Initial Commit

---
+++ DribbleChalange/DribbleChalangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleChalangeApp.swift
+//  DribbleChalange
+//
+//  Created by Авазбек Надырбек уулу on 14.07.25.
+//
+
+import SwiftUI
+
+@main
+struct DribbleChalangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
